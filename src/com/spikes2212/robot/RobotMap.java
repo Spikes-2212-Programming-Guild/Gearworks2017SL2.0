38bf3246195e2ac1426bcc87e7c348e00720a391
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 12:19:48 +0000

    added CAN constants to RobotMap

--- src/com/spikes2212/robot/RobotMap.java
+++ src/com/spikes2212/robot/RobotMap.java
@@ -7,4 +7,10 @@ package com.spikes2212.robot;
  * floating around.
  */
 public class RobotMap {
+    public static class CAN {
+        public static final int LEFT_ONE = 0;
+        public static final int LEFT_TWO = 1;
+        public static final int RIGHT_ONE = 2;
+        public static final int RIGHT_TWO = 3;
+    }
 }
