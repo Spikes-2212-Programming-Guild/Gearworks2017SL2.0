5a8c291343320c9ab85dfbb9c9386a7d0c7e98ec
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 11:52:35 +0000

    added empty classes for all the subsystems

--- src/com/spikes2212/robot/Subsystems.java
+++ src/com/spikes2212/robot/Subsystems.java
@@ -1,4 +1,15 @@
 package com.spikes2212.robot;
 
 public class Subsystems {
+    public static class Drivetrain {
+
+    }
+
+    public static class Shooter {
+
+    }
+
+    public static class GearSystems {
+
+    }
 }
