006023c676ca144037b54da5c229ac046767c30d
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 18:06:34 +0000

    added 5 position functionality to GearLift

--- src/com/spikes2212/robot/subsystems/GearLift.java
+++ src/com/spikes2212/robot/subsystems/GearLift.java
@@ -1,16 +1,59 @@
 package com.spikes2212.robot.subsystems;
 
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.BasicSubsystem;
+import com.spikes2212.genericsubsystems.limitationFunctions.TwoLimits;
 import edu.wpi.first.wpilibj.Encoder;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class GearLift extends BasicSubsystem {
+
+    public static final Supplier<Double> middlePositionValue = ConstantHandler.addConstantDouble(
+            "Gear Lift Middle Position Value",
+            0
+    );
+
+    public enum LiftPosition {
+         UP, DOWN, MIDDLE, MIDDLE_DOWN, MIDDLE_UP
+    }
+
+    private Encoder liftEncoder;
+
+    private Supplier<Boolean> maxLimit, minLimit;
+
+
     public GearLift(Consumer<Double> speedConsumer,
                     Supplier<Boolean> maxLimit,
                     Supplier<Boolean> minLimit,
                     Encoder liftEncoder) {
-        super(speedConsumer, maxLimit, minLimit);
+        super(speedConsumer, new TwoLimits(maxLimit, minLimit));
+        this.liftEncoder = liftEncoder;
+        this.maxLimit = maxLimit;
+        this.minLimit = () -> {
+            if (minLimit.get()){
+                resetEncoder();
+                return true;
+            }
+            return false;
+        };
+
+    }
+
+    public LiftPosition getPosition() {
+        if (liftEncoder.get() == middlePositionValue.get())
+            return LiftPosition.MIDDLE;
+        if (minLimit.get())
+            return LiftPosition.DOWN;
+        if (maxLimit.get())
+            return LiftPosition.UP;
+        if (liftEncoder.get() < middlePositionValue.get())
+            return LiftPosition.MIDDLE_DOWN;
+        return LiftPosition.MIDDLE_UP;
+    }
+
+    public void resetEncoder() {
+        liftEncoder.reset();
     }
 }
