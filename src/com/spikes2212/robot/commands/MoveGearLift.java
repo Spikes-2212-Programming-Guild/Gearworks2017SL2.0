aaa404082ddbd00edbee8e8b6344607b4784ddc6
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 19:23:56 +0000

    overridden isFinished in MoveGearLift

--- src/com/spikes2212/robot/commands/MoveGearLift.java
+++ src/com/spikes2212/robot/commands/MoveGearLift.java
@@ -28,5 +28,9 @@ public class MoveGearLift extends MoveBasicSubsystem {
         };
     }
 
+    @Override
+    protected boolean isFinished() {
+        return ((GearLift) basicSubsystem).getPosition() == targetPosition || isTimedOut();
 
+    }
 }
