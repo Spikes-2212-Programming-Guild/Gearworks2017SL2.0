d2735dffd96719dcb442e561cbd84f4892e75bef
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 19:04:55 +0000

    added LiftEncoder to Subsystems.GearSystems

--- src/com/spikes2212/robot/Subsystems.java
+++ src/com/spikes2212/robot/Subsystems.java
@@ -3,6 +3,7 @@ package com.spikes2212.robot;
 import com.ctre.CANTalon;
 import com.spikes2212.utils.DoubleSpeedcontroller;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Subsystems {
@@ -24,5 +25,7 @@ public class Subsystems {
         public static SpeedController gearLiftSP = new CANTalon(RobotMap.CAN.LIFT);
         public static DigitalInput maxLimit = new DigitalInput(RobotMap.DIO.LIFT_LIMIT_MAX);
         public static DigitalInput minLimit = new DigitalInput(RobotMap.DIO.LIFT_LIMIT_MIN);
+        public static Encoder liftEncoder = new Encoder(RobotMap.DIO.LIFT_ENCODER_ONE,
+                RobotMap.DIO.LIFT_ENCODER_TWO);
     }
 }
