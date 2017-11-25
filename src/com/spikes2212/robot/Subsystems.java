a1012d946c4b6b69455288f3ebcbb8d3d2559ce8
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-25 12:29:49 +0000

    defined SpeedControllers for Drivetrain

--- src/com/spikes2212/robot/Subsystems.java
+++ src/com/spikes2212/robot/Subsystems.java
@@ -1,8 +1,18 @@
 package com.spikes2212.robot;
 
+import com.ctre.CANTalon;
+import com.spikes2212.utils.DoubleSpeedcontroller;
+import edu.wpi.first.wpilibj.SpeedController;
+
 public class Subsystems {
     public static class Drivetrain {
+        public static SpeedController leftSP = new DoubleSpeedcontroller(
+            new CANTalon(RobotMap.CAN.LEFT_ONE), new CANTalon(RobotMap.CAN.LEFT_TWO)
+        );
 
+        public static SpeedController rightSP = new DoubleSpeedcontroller(
+                new CANTalon(RobotMap.CAN.RIGHT_ONE), new CANTalon(RobotMap.CAN.RIGHT_TWO)
+        );
     }
 
     public static class Shooter {
