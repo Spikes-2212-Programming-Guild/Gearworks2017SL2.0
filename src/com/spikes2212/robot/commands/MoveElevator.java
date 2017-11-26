95a7c78dc4df452911d84cd8496c5421c8873a2a
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-26 20:35:43 +0000

    moved SPEED to Elevator

--- src/com/spikes2212/robot/commands/MoveElevator.java
+++ src/com/spikes2212/robot/commands/MoveElevator.java
@@ -4,21 +4,22 @@ package com.spikes2212.robot.commands;
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.robot.subsystems.Elevator;
+import com.sun.xml.internal.xsom.impl.Ref;
 
 import java.util.function.Supplier;
 
-public class MoveGearElevator extends MoveBasicSubsystem {
+public class MoveElevator extends MoveBasicSubsystem {
 
     private Elevator.ElevatorState targetPosition;
 
-    public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Elevator-Speed", 0.2);
 
-    public MoveGearElevator(Elevator elevator, Elevator.ElevatorState targetPosition) {
-        super(elevator, SPEED);
+
+    public MoveElevator(Elevator elevator, Elevator.ElevatorState targetPosition) {
+        super(elevator, Elevator.SPEED);
         this.targetPosition = targetPosition;
 
         this.speedSupplier = () -> (elevator.getPosition().getIndex() > targetPosition.getIndex())
-                ? SPEED.get() : -SPEED.get();
+                ? Elevator.SPEED.get() : -Elevator.SPEED.get();
     }
 
     @Override
