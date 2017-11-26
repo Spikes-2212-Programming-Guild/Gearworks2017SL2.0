b1dec1ef147a5943092f604abd1d8074f66a8a0c
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-26 20:31:02 +0000

    fixed MoveGearElevator

---
+++ src/com/spikes2212/robot/commands/MoveGearElevator.java
@@ -0,0 +1,35 @@
+package com.spikes2212.robot.commands;
+
+
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.robot.subsystems.Elevator;
+
+import java.util.function.Supplier;
+
+public class MoveGearElevator extends MoveBasicSubsystem {
+
+    private Elevator.ElevatorState targetPosition;
+
+    public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Elevator-Speed", 0.2);
+
+    public MoveGearElevator(Elevator elevator,
+                            Elevator.ElevatorState targetPosition) {
+        super(elevator, SPEED);
+        this.targetPosition = targetPosition;
+
+        this.speedSupplier = () -> (elevator.getPosition().getIndex() > targetPosition.getIndex())
+                ? SPEED.get() : -SPEED.get();
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return ((Elevator) basicSubsystem).getPosition() == targetPosition || isTimedOut();
+
+    }
+}
