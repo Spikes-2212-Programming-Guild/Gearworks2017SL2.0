1054f19f7be2de1f453aab54df6aca83b67690da
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-26 17:18:56 +0000

    renamed Drivetrain constants

--- src/com/spikes2212/robot/Subsystems.java
+++ src/com/spikes2212/robot/Subsystems.java
@@ -7,11 +7,11 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class Subsystems {
     public static class Drivetrain {
         public static SpeedController leftSP = new DoubleSpeedcontroller(
-            new CANTalon(RobotMap.CAN.LEFT_ONE), new CANTalon(RobotMap.CAN.LEFT_TWO)
+            new CANTalon(RobotMap.CAN.DRIVETRAIN_LEFT_1), new CANTalon(RobotMap.CAN.DRIVETRAIN_LEFT_2)
         );
 
         public static SpeedController rightSP = new DoubleSpeedcontroller(
-                new CANTalon(RobotMap.CAN.RIGHT_ONE), new CANTalon(RobotMap.CAN.RIGHT_TWO)
+                new CANTalon(RobotMap.CAN.DRIVETRAIN_RIGHT_1), new CANTalon(RobotMap.CAN.DRIVETRAIN_RIGHT_2)
         );
     }
 
