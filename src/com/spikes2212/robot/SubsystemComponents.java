21bf2bbe55f456f195922784b91236ed665c2bd4
TheArchNemesis <simon.kharmatsky@gmail.com>
2017-11-28 18:26:11 +0000

    added components to GearPickerComponents

--- src/com/spikes2212/robot/SubsystemComponents.java
+++ src/com/spikes2212/robot/SubsystemComponents.java
@@ -2,6 +2,7 @@ package com.spikes2212.robot;
 
 import com.ctre.CANTalon;
 import com.spikes2212.utils.DoubleSpeedcontroller;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -29,5 +30,13 @@ public class SubsystemComponents {
         public static SpeedController rollerGripperSP = new CANTalon(RobotMap.CAN.ROLLER_GRIPPER);
         public static DigitalInput colorSensor =
                 new DigitalInput(RobotMap.DIO.ROLLER_GRIPPER_COLOR_SENSOR);
+    }
+
+    public static class GearPickerComponents {
+        public static SpeedController gearPickerSP = new CANTalon(RobotMap.CAN.GEAR_PICKER);
+        public static DigitalInput maxLimit = new DigitalInput(RobotMap.DIO.GEAR_PICKER_LIMIT_MAX);
+        public static DigitalInput minLimit = new DigitalInput(RobotMap.DIO.GEAR_PICKER_LIMIT_MIN);
+        public static AnalogPotentiometer potentiometer =
+                new AnalogPotentiometer(RobotMap.AnalogInput.GEAR_PICKER_PATENTIOMENTER);
     }
 }
